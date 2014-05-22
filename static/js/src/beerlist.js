d5abcea57bdf34e0d22013caac58079834e2e971
Atle Frenvik Sveen <atle.sveen@bouvet.no>
2014-05-22 16:15:50 +0200

    jslint

--- static/js/src/beerlist.js
+++ static/js/src/beerlist.js
@@ -1,146 +1,145 @@
 var BL = {};
 (function (ns) {
-  'use strict';
-  
-  var Beer = Backbone.Model.extend({
+    'use strict';
 
-    defaults: {
-        name: '',
-        brewery: '',
-        abv: '',
-        score: '',
-        amount: 1
-    },
+    var Beer = Backbone.Model.extend({
 
-    url: function () {
-        var url = '/api/beers/';
-        if (!this.isNew()) {
-            url += this.get('id');
+        defaults: {
+            name: '',
+            brewery: '',
+            abv: '',
+            score: '',
+            amount: 1
+        },
+
+        url: function () {
+            var url = '/api/beers/';
+            if (!this.isNew()) {
+                url += this.get('id');
+            }
+            return url;
         }
-        return url;
-    }
 
-  });
+    });
 
-  ns.Beers = Backbone.Collection.extend({
-    model: Beer
-  });
+    ns.Beers = Backbone.Collection.extend({
+        model: Beer
+    });
 
-  var BeerEditView = Backbone.View.extend({
-    
-    tagName: 'tr',
+    var BeerEditView = Backbone.View.extend({
 
-    template: $('#beer_edit_template').html(),
+        tagName: 'tr',
 
-    events: {
-        'click #save': 'save'
-    },
+        template: $('#beer_edit_template').html(),
 
-    initialize: function () {
-        this.model.on('sync', this.saved, this);
-    },
+        events: {
+            'click #save': 'save'
+        },
 
-    render: function () {
-        this.$el.html(_.template(this.template, this.model.toJSON()));    
-        return this;
-    },
+        initialize: function () {
+            this.model.on('sync', this.saved, this);
+        },
 
-    save: function () {
-        console.log("SAVE!")
-        this.model.set({
-            name: this.$('[name="name"]').val(),
-            brewery: this.$('[name="brewery"]').val(),
-            abv: this.$('[name="abv"]').val(),
-            score: this.$('[name="score"]').val(),
-            amount: this.$('[name="amount"]').val()
-        });
+        render: function () {
+            this.$el.html(_.template(this.template, this.model.toJSON()));
+            return this;
+        },
 
-        this.model.save();
-    },
+        save: function () {
+            this.model.set({
+                name: this.$('[name="name"]').val(),
+                brewery: this.$('[name="brewery"]').val(),
+                abv: this.$('[name="abv"]').val(),
+                score: this.$('[name="score"]').val(),
+                amount: this.$('[name="amount"]').val()
+            });
 
-    saved: function () {
-        console.log('saved!');
-        if (this.collection) {
-            this.collection.add(this.model);
+            this.model.save();
+        },
+
+        saved: function () {
+            console.log('saved!');
+            if (this.collection) {
+                this.collection.add(this.model);
+            }
+            this.remove();
         }
-        this.remove();
-    }
 
-  });
+    });
 
-  var BeerView = Backbone.View.extend({
-    
-    tagName: 'tr',
+    var BeerView = Backbone.View.extend({
 
-    template: $('#beer_row_template').html(),
+        tagName: 'tr',
 
-    events: {
-        'click #delete': 'destroy',
-        'click #edit': 'edit'
-    },
+        template: $('#beer_row_template').html(),
 
-    initialize: function () {
-        this.model.on('destroy', this.remove, this);
-        this.model.on('sync', this.render, this);
-    },
+        events: {
+            'click #delete': 'destroy',
+            'click #edit': 'edit'
+        },
 
-    render: function () {
-        this.$el.show();
-        this.$el.html(_.template(this.template, this.model.toJSON()));
-        return this;
-    },
+        initialize: function () {
+            this.model.on('destroy', this.remove, this);
+            this.model.on('sync', this.render, this);
+        },
 
-    destroy: function () {
-        this.model.destroy({'wait': true});
-    },
+        render: function () {
+            this.$el.show();
+            this.$el.html(_.template(this.template, this.model.toJSON()));
+            return this;
+        },
 
-    edit: function () {
-        this.$el.hide();
-        this.$el.after(
-            new BeerEditView({
+        destroy: function () {
+            this.model.destroy({'wait': true});
+        },
+
+        edit: function () {
+            this.$el.hide();
+            this.$el.after(
+                new BeerEditView({
+                    collection: this.collection,
+                    model: this.model
+                }).render().$el
+            );
+        }
+
+    });
+
+    ns.BeerTableView = Backbone.View.extend({
+
+        tagName: 'table',
+
+        className: 'table table-striped',
+
+        template: $('#beer_table_template').html(),
+
+        events: {
+            'click #add': 'addBeer'
+        },
+
+        initialize: function () {
+            this.collection.on('add', this.beerAdded, this);
+        },
+
+        render: function () {
+            this.$el.html(_.template(this.template));
+            this.$('tbody').append(this.collection.map(function (beer) {
+                return new BeerView({model: beer}).render().$el;
+            }));
+            return this;
+        },
+
+        addBeer: function () {
+            this.$('tbody').append(new BeerEditView({
                 collection: this.collection,
-                model: this.model
-            }).render().$el
-        );
-    }
+                model: new Beer()
+            }).render().$el);
+        },
 
-  });
+        beerAdded: function (model) {
+            this.$('tbody').append(new BeerView({model: model}).render().$el);
+        }
 
-  ns.BeerTableView = Backbone.View.extend({
-
-    tagName: 'table',
-
-    className: 'table table-striped',
-
-    template: $('#beer_table_template').html(),
-
-    events: {
-        'click #add': 'addBeer'
-    },
-
-    initialize: function () {
-        this.collection.on('add', this.beerAdded, this);
-    },
-
-    render: function () {
-        this.$el.html(_.template(this.template));
-        this.$('tbody').append(this.collection.map(function (beer) {
-            return new BeerView({model: beer}).render().$el;
-        }));
-        return this;
-    },
-
-    addBeer: function () {
-        this.$('tbody').append(new BeerEditView({
-            collection: this.collection,
-            model: new Beer()
-        }).render().$el);
-    },
-
-    beerAdded: function (model) {
-        this.$('tbody').append(new BeerView({model: model}).render().$el);
-    }
-
-  });
+    });
 
 }(BL));
